a5a258d6c273d0aef9d63ea7837f3bed0380b8d6
Pawel Rugala <webdevpablo@gmail.com>
2022-06-04 23:13:34 +0200

    test: express mixed with react

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+ return (
+  <div>
+   <h1 className='text-7xl text-red-800'>Client app</h1>
+   <p className='text-slate-600 cursor-pointer'>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora velit et
+    tenetur ullam adipisci suscipit nostrum quae odit iste animi! Ab in ipsa eum
+    quae ex perspiciatis! Quaerat, aliquam praesentium?
+   </p>
+  </div>
+ )
+}
+
+export default App
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use((req, res, next) => {
+ const hostname = req.hostname
+ const client = hostname.split('.')[0]
+ if (client === 'localhost') {
+  req.clientUrl = null
+ } else {
+  req.clientUrl = client
+ }
+ next()
+})
+
+app.use(express.static('public'))
+app.use(express.static('client/dist'))
+
 app.get('/', (req, res) => {
- res.send('Hello World!')
+ if (req.clientUrl) {
+  res.sendFile(`${__dirname}/public/${req.clientUrl}/index.html`)
+ } else {
+  res.sendFile(`${__dirname}/client/dist/index.html`)
+ }
 })
 
 app.listen(3000, () => {
  console.log('Example app listening on port 3000!')
- console.log('http://localhost:3000/')
 })
